09521f9347ef73d982bd0348726c3803e4bd19f6
hiten36 <hitenkhatri14@gmail.com>
2023-12-06 03:16:04 +0530

    cha

--- index.js
+++ index.js
@@ -19,135 +19,135 @@ app.post("/contact", async (req, res) => {
 
   console.log({ name, email, phone, service, message });
 
-  // let transporter = nodemailer.createTransport({
-  //   host: "mail.kusheldigi.com",
-  //   port: 465,
-  //   secure: true, // true for 465, false for other ports
-  //   auth: {
-  //     user: "info@kusheldigi.com",
-  //     pass: "Kusheldigiinfo@2025",
-  //   },
-  //   from: "info@kusheldigi.com",
-  //   tls: {
-  //     rejectUnauthorized: false,
-  //   },
-  // });
+  let transporter = nodemailer.createTransport({
+    host: "mail.kusheldigi.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "info@kusheldigi.com",
+      pass: "Kusheldigiinfo@2025",
+    },
+    from: "info@kusheldigi.com",
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-  // // const htmlToSend = template(replacements);
+  // const htmlToSend = template(replacements);
 
-  // // send mail with defined transport object
-  // let info = await transporter.sendMail({
-  //   from: '"Kushel Digi Solutions" <info@kusheldigi.com>',
-  //   to: "info@kusheldigi.com",
-  //   subject: "Contact Form",
-  //   text: `
-  //       <div>
-  //           <div>Name: ${name}</div>
-  //           <div>Phone: ${phone}</div>
-  //           <div>Email: ${email}</div>
-  //           <div>Service: ${service}</div>
-  //           <div>Message: ${message}</div>
-  //       </div>
-  //   `,
-  //   html: `
-  //           <div>
-  //               <div>Name: ${name}</div>
-  //               <div>Phone: ${phone}</div>
-  //               <div>Email: ${email}</div>
-  //               <div>Service: ${service}</div>
-  //           <div>Message: ${message}</div>
-  //           </div>
-  //       `,
-  // });
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Kushel Digi Solutions" <info@kusheldigi.com>',
+    to: "info@kusheldigi.com",
+    subject: "Contact Form",
+    text: `
+        <div>
+            <div>Name: ${name}</div>
+            <div>Phone: ${phone}</div>
+            <div>Email: ${email}</div>
+            <div>Service: ${service}</div>
+            <div>Message: ${message}</div>
+        </div>
+    `,
+    html: `
+            <div>
+                <div>Name: ${name}</div>
+                <div>Phone: ${phone}</div>
+                <div>Email: ${email}</div>
+                <div>Service: ${service}</div>
+            <div>Message: ${message}</div>
+            </div>
+        `,
+  });
 
-  // let info1 = await transporter.sendMail({
-  //   from: '"Kushel Digi Solutions" <info@kusheldigi.com>',
-  //   to: email,
-  //   subject: "Contact Form",
-  //   text: `
-  //     Thank you
-  //   `,
-  //   html: `
-  //           <div>
-  //               <div>Thank you! we will get back to you</div>
-  //           </div>
-  //       `,
-  // });
+  let info1 = await transporter.sendMail({
+    from: '"Kushel Digi Solutions" <info@kusheldigi.com>',
+    to: email,
+    subject: "Contact Form",
+    text: `
+      Thank you
+    `,
+    html: `
+            <div>
+                <div>Thank you! we will get back to you</div>
+            </div>
+        `,
+  });
 
-  // let resp1 = await fetch(`https://accounts.zoho.in/oauth/v2/token?grant_type=refresh_token&client_id=1000.FS0PE9O76Z2VG1XDJFGG49O4J77ZKF&client_secret=e49d2b9e743e403ebba076fd28a05a80f6e5815833&refresh_token=1000.7cabfb8e30f390c31275783a09f4b907.2aea28f36e7defada84b8e4dc38ce432`, {
-  //   method: 'POST'
-  // });
-  // let data1 = await resp1.json();
-  // // console.log(data1);
-  // let resp2 = await fetch("https://www.zohoapis.in/crm/v4/Leads", {
-  //   method: 'POST',
-  //   headers: {
-  //     Authorization: `Bearer ${data1.access_token}`
-  //   },
-  //   body: JSON.stringify({
-  //     "data": [
-  //       {
-  //         "Department": service,
-  //         "First_Name": name,
-  //         "Last_Name": "-",
-  //         "Email": email,
-  //         "Description": message,
-  //         "Phone": phone
-  //       }
-  //     ]
-  //   })
-  // });
-  // let data2 = await resp2.json();
+  let resp1 = await fetch(`https://accounts.zoho.in/oauth/v2/token?grant_type=refresh_token&client_id=1000.FS0PE9O76Z2VG1XDJFGG49O4J77ZKF&client_secret=e49d2b9e743e403ebba076fd28a05a80f6e5815833&refresh_token=1000.7cabfb8e30f390c31275783a09f4b907.2aea28f36e7defada84b8e4dc38ce432`, {
+    method: 'POST'
+  });
+  let data1 = await resp1.json();
+  // console.log(data1);
+  let resp2 = await fetch("https://www.zohoapis.in/crm/v4/Leads", {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${data1.access_token}`
+    },
+    body: JSON.stringify({
+      "data": [
+        {
+          "Department": service,
+          "First_Name": name,
+          "Last_Name": "-",
+          "Email": email,
+          "Description": message,
+          "Phone": phone
+        }
+      ]
+    })
+  });
+  let data2 = await resp2.json();
 
   res.json({ success: true, message: "Thank You! we will get back you shortly" });
 });
 
 app.post("/contact1", async (req, res) => {
   const { company1, name1, email1, phone1, service1, message1 } = req.body;
-  console.log({ company1, name1, email1, phone1, service1, message1 });
+  // console.log({ company1, name1, email1, phone1, service1, message1 });
 
-//   let transporter = nodemailer.createTransport({
-//     host: "mail.kusheldigi.com",
-//     port: 465,
-//     secure: true, // true for 465, false for other ports
-//     auth: {
-//       user: "info@kusheldigi.com",
-//       pass: "Kusheldigiinfo@2025",
-//     },
-//     from: "info@kusheldigi.com",
-//     tls: {
-//       rejectUnauthorized: false,
-//     },
-//   });
+  let transporter = nodemailer.createTransport({
+    host: "mail.kusheldigi.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "info@kusheldigi.com",
+      pass: "Kusheldigiinfo@2025",
+    },
+    from: "info@kusheldigi.com",
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-//   // const htmlToSend = template(replacements);
+  // const htmlToSend = template(replacements);
 
-//   // send mail with defined transport object
-//   let info1 = await transporter.sendMail({
-//     from: '"Kushel Digi Solutions" <info@kusheldigi.com>',
-//     to: "info@kusheldigi.com",
-//     subject: "Contact Form",
-//     text: `
-//         <div>
-//             <div>Company: ${company1}</div>
-//             <div>Name: ${name1}</div>
-//             <div>Email: ${email1}</div>
-//             <div>Phone: ${phone1}</div>
-//             <div>Service: ${service1}</div>
-//             <div>Message: ${message1}</div>
-//         </div>
-//     `,
-//     html: `
-//     <div>
-//     <div>Company: ${company1}</div>
-//     <div>Name: ${name1}</div>
-//     <div>Email: ${email1}</div>
-//     <div>Phone: ${phone1}</div>
-//     <div>Service: ${service1}</div>
-//     <div>Message: ${message1}</div>
-// </div>
-//         `,
-//   });
+  // send mail with defined transport object
+  let info1 = await transporter.sendMail({
+    from: '"Kushel Digi Solutions" <info@kusheldigi.com>',
+    to: "info@kusheldigi.com",
+    subject: "Contact Form",
+    text: `
+        <div>
+            <div>Company: ${company1}</div>
+            <div>Name: ${name1}</div>
+            <div>Email: ${email1}</div>
+            <div>Phone: ${phone1}</div>
+            <div>Service: ${service1}</div>
+            <div>Message: ${message1}</div>
+        </div>
+    `,
+    html: `
+    <div>
+    <div>Company: ${company1}</div>
+    <div>Name: ${name1}</div>
+    <div>Email: ${email1}</div>
+    <div>Phone: ${phone1}</div>
+    <div>Service: ${service1}</div>
+    <div>Message: ${message1}</div>
+</div>
+        `,
+  });
 
 //   let info2 = await transporter.sendMail({
 //     from: '"Kushel Digi Solutions" <info@kusheldigi.com>',
@@ -188,7 +188,7 @@ app.post("/contact1", async (req, res) => {
 //   });
 //   let data2 = await resp2.json();
 
-  res.json({ success: true, message: "Thank You! we will get back you shortly" });
+  res.json({ success: true, message: "Thank You! we will get back you shortly", data: { company1, name1, email1, phone1, service1, message1 } });
 });
 
 app.post("/contact2", async (req, res) => {
